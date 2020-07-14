05d59b4d34867080109d22f8ab030116662f9e18
RinHizakura <s921975628@gmail.com>
2020-07-14 22:44:02 +0800

    Implement copy on write for xs_cpy and xs_concat

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
+COPS = -Wall -Wextra -Werror 
+
 all: xs.o
 
 xs.o: xs.c
-	gcc -o xs -std=gnu11 xs.c
+	gcc $(COPS) -o xs -std=gnu11 xs.c
--- xs.c
+++ xs.c
@@ -1,199 +1,161 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
+#include "xs.h"
 
-typedef union {
-    /* allow strings up to 15 bytes to stay on the stack
-     * use the last byte as a null terminator and to store flags
-     * much like fbstring:
-     * https://github.com/facebook/folly/blob/master/folly/docs/FBString.md
-     */
-    char data[16];
-
-    struct {
-        uint8_t filler[15],
-            /* how many free bytes in this stack allocated string
-             * same idea as fbstring
-             */
-            space_left : 4,
-            /* if it is on heap, set to 1 */
-            is_ptr : 1, flag1 : 1, flag2 : 1, flag3 : 1;
-    };
-
-    /* heap allocated */
-    struct {
-        char *ptr;
-        /* supports strings up to 2^54 - 1 bytes */
-        size_t size : 54,
-            /* capacity is always a power of 2 (unsigned)-1 */
-            capacity : 6;
-        /* the last 4 bits are important flags */
-    };
-} xs;
-
-static inline bool xs_is_ptr(const xs *x) { return x->is_ptr; }
-static inline size_t xs_size(const xs *x)
-{
-    return xs_is_ptr(x) ? x->size : 15 - x->space_left;
+xs *xs_new(xs *x, const void *p) {
+  *x = xs_literal_empty();
+  size_t len = strlen(p) + 1;
+  if (len > 16) {
+    x->capacity = ilog2(len) + 1;
+    x->size = len - 1;
+    x->is_ptr = true;
+    x->ptr = malloc(((size_t)1 << x->capacity) + OFFSET) + OFFSET;
+    memcpy(x->ptr, p, len);
+    XS_INIT_REFCNT(x);
+  } else {
+    memcpy(x->data, p, len);
+    x->space_left = 15 - (len - 1);
+  }
+  return x;
 }
-static inline char *xs_data(const xs *x)
-{
-    return xs_is_ptr(x) ? (char *) x->ptr : (char *) x->data;
-}
-static inline size_t xs_capacity(const xs *x)
-{
-    return xs_is_ptr(x) ? ((size_t) 1 << x->capacity) - 1 : 15;
-}
-
-#define xs_literal_empty() \
-    (xs) { .space_left = 15 }
-
-static inline int ilog2(uint32_t n) { return 32 - __builtin_clz(n) - 1; }
-
-xs *xs_new(xs *x, const void *p)
-{
-    *x = xs_literal_empty();
-    size_t len = strlen(p) + 1;
-    if (len > 16) {
-        x->capacity = ilog2(len) + 1;
-        x->size = len - 1;
-        x->is_ptr = true;
-        x->ptr = malloc((size_t) 1 << x->capacity);
-        memcpy(x->ptr, p, len);
-    } else {
-        memcpy(x->data, p, len);
-        x->space_left = 15 - (len - 1);
-    }
-    return x;
-}
-
-/* Memory leaks happen if the string is too long but it is still useful for
- * short strings.
- * "" causes a compile-time error if x is not a string literal or too long.
- */
-#define xs_tmp(x)                                          \
-    ((void) ((struct {                                     \
-         _Static_assert(sizeof(x) <= 16, "it is too big"); \
-         int dummy;                                        \
-     }){1}),                                               \
-     xs_new(&xs_literal_empty(), "" x))
 
 /* grow up to specified size */
-xs *xs_grow(xs *x, size_t len)
-{
-    if (len <= xs_capacity(x))
-        return x;
-    len = ilog2(len) + 1;
-    if (xs_is_ptr(x))
-        x->ptr = realloc(x->ptr, (size_t) 1 << len);
-    else {
-        char buf[16];
-        memcpy(buf, x->data, 16);
-        x->ptr = malloc((size_t) 1 << len);
-        memcpy(x->ptr, buf, 16);
-    }
-    x->is_ptr = true;
-    x->capacity = len;
+xs *xs_grow(xs *x, size_t len) {
+  if (len <= xs_capacity(x))
     return x;
+  len = ilog2(len) + 1;
+  if (xs_is_ptr(x))
+    x->ptr = realloc(x->ptr, ((size_t)1 << len) + OFFSET) + OFFSET;
+  else {
+    char buf[16];
+    memcpy(buf, x->data, 16);
+    x->ptr = malloc(((size_t)1 << len) + OFFSET) + OFFSET;
+    memcpy(x->ptr, buf, 16);
+  }
+  x->is_ptr = true;
+  x->capacity = len;
+  XS_INIT_REFCNT(x);
+  return x;
 }
 
-static inline xs *xs_newempty(xs *x)
-{
-    *x = xs_literal_empty();
+void xs_cow(xs *x) {
+  if (XS_GET_REFCNT(x) > 1) {
+    XS_DECR_REFCNT(x);
+    xs_new(x, xs_data(x));
+  }
+}
+
+xs *xs_concat(xs *string, const xs *prefix, const xs *suffix) {
+  // before we start, check if our string is a COW string
+  xs_cow(string);
+
+  size_t pres = xs_size(prefix), sufs = xs_size(suffix), size = xs_size(string),
+         capacity = xs_capacity(string);
+
+  char *pre = xs_data(prefix), *suf = xs_data(suffix), *data = xs_data(string);
+
+  if (size + pres + sufs <= capacity) {
+    memmove(data + pres, data, size);
+    memcpy(data, pre, pres);
+    memcpy(data + pres + size, suf, sufs + 1);
+    string->space_left = 15 - (size + pres + sufs);
+  } else {
+    xs tmps = xs_literal_empty();
+    xs_grow(&tmps, size + pres + sufs);
+    char *tmpdata = xs_data(&tmps);
+    memcpy(tmpdata + pres, data, size);
+    memcpy(tmpdata, pre, pres);
+    memcpy(tmpdata + pres + size, suf, sufs + 1);
+    xs_free(string);
+    *string = tmps;
+    string->size = size + pres + sufs;
+  }
+  return string;
+}
+
+xs *xs_trim(xs *x, const char *trimset) {
+  if (!trimset[0])
     return x;
-}
 
-static inline xs *xs_free(xs *x)
-{
-    if (xs_is_ptr(x))
-        free(xs_data(x));
-    return xs_newempty(x);
-}
+  char *dataptr = xs_data(x), *orig = dataptr;
 
-xs *xs_concat(xs *string, const xs *prefix, const xs *suffix)
-{
-    size_t pres = xs_size(prefix), sufs = xs_size(suffix),
-           size = xs_size(string), capacity = xs_capacity(string);
+  /* similar to strspn/strpbrk but it operates on binary data */
+  uint8_t mask[32] = {0};
 
-    char *pre = xs_data(prefix), *suf = xs_data(suffix),
-         *data = xs_data(string);
+#define check_bit(byte) (mask[(uint8_t)byte / 8] & 1 << (uint8_t)byte % 8)
+#define set_bit(byte) (mask[(uint8_t)byte / 8] |= 1 << (uint8_t)byte % 8)
 
-    if (size + pres + sufs <= capacity) {
-        memmove(data + pres, data, size);
-        memcpy(data, pre, pres);
-        memcpy(data + pres + size, suf, sufs + 1);
-        string->space_left = 15 - (size + pres + sufs);
-    } else {
-        xs tmps = xs_literal_empty();
-        xs_grow(&tmps, size + pres + sufs);
-        char *tmpdata = xs_data(&tmps);
-        memcpy(tmpdata + pres, data, size);
-        memcpy(tmpdata, pre, pres);
-        memcpy(tmpdata + pres + size, suf, sufs + 1);
-        xs_free(string);
-        *string = tmps;
-        string->size = size + pres + sufs;
-    }
-    return string;
-}
+  size_t i, slen = xs_size(x), trimlen = strlen(trimset);
 
-xs *xs_trim(xs *x, const char *trimset)
-{
-    if (!trimset[0])
-        return x;
+  for (i = 0; i < trimlen; i++)
+    set_bit(trimset[i]);
+  for (i = 0; i < slen; i++)
+    if (!check_bit(dataptr[i]))
+      break;
+  for (; slen > 0; slen--)
+    if (!check_bit(dataptr[slen - 1]))
+      break;
+  dataptr += i;
+  slen -= i;
 
-    char *dataptr = xs_data(x), *orig = dataptr;
+  /* reserved space as a buffer on the heap.
+   * Do not reallocate immediately. Instead, reuse it as possible.
+   * Do not shrink to in place if < 16 bytes.
+   */
+  memmove(orig, dataptr, slen);
+  /* do not dirty memory unless it is needed */
+  if (orig[slen])
+    orig[slen] = 0;
 
-    /* similar to strspn/strpbrk but it operates on binary data */
-    uint8_t mask[32] = {0};
-
-#define check_bit(byte) (mask[(uint8_t) byte / 8] & 1 << (uint8_t) byte % 8)
-#define set_bit(byte) (mask[(uint8_t) byte / 8] |= 1 << (uint8_t) byte % 8)
-
-    size_t i, slen = xs_size(x), trimlen = strlen(trimset);
-
-    for (i = 0; i < trimlen; i++)
-        set_bit(trimset[i]);
-    for (i = 0; i < slen; i++)
-        if (!check_bit(dataptr[i]))
-            break;
-    for (; slen > 0; slen--)
-        if (!check_bit(dataptr[slen - 1]))
-            break;
-    dataptr += i;
-    slen -= i;
-
-    /* reserved space as a buffer on the heap.
-     * Do not reallocate immediately. Instead, reuse it as possible.
-     * Do not shrink to in place if < 16 bytes.
-     */
-    memmove(orig, dataptr, slen);
-    /* do not dirty memory unless it is needed */
-    if (orig[slen])
-        orig[slen] = 0;
-
-    if (xs_is_ptr(x))
-        x->size = slen;
-    else
-        x->space_left = 15 - slen;
-    return x;
+  if (xs_is_ptr(x))
+    x->size = slen;
+  else
+    x->space_left = 15 - slen;
+  return x;
 #undef check_bit
 #undef set_bit
 }
 
-#include <stdio.h>
+xs *xs_cpy(xs *dest, xs *src) {
+  // If dest was referenced to others, decrease the reference count first
+  if (XS_GET_REFCNT(dest) > 1)
+    XS_DECR_REFCNT(dest);
 
-int main()
-{
-    xs string = *xs_tmp("\n foobarbar \n\n\n");
-    xs_trim(&string, "\n ");
-    printf("[%s] : %2zu\n", xs_data(&string), xs_size(&string));
+  // If src is long string, just make a copy
+  if (xs_is_ptr(src)) {
+    dest->is_ptr = true;
+    XS_INCR_REFCNT(src);
+    *dest = *src;
+  }
+  // Else just simply memcpy
+  else {
+    dest->is_ptr = false; // set dest's flag to short string
+    size_t len = xs_size(src);
+    memcpy(xs_data(dest), xs_data(src), len);
+    dest->space_left = 15 - len;
+  }
+  return dest;
+}
 
-    xs prefix = *xs_tmp("((("), suffix = *xs_tmp(")))");
-    xs_concat(&string, &prefix, &suffix);
-    printf("[%s] : %2zu\n", xs_data(&string), xs_size(&string));
+void test_cpy() {
 
-    return 0;
+  printf("===== Test1 =====\n");
+  xs src1;
+  xs s1, s2;
+  xs_new(&src1, "Happy Lucky Smile Yeah!!!!!!");
+  xs_cpy(&s1, &src1);
+  printf("Befor cpy to s2, s1: %s %ld\n", xs_data(&s1), XS_GET_REFCNT((&s1)));
+  xs_cpy(&s2, &src1);
+  printf("After cpy to s2, s1: %s %ld\n", xs_data(&s1), XS_GET_REFCNT((&s1)));
+  printf("Now s2: %s %ld\n", xs_data(&s2), XS_GET_REFCNT((&s2)));
+
+  printf("===== Test2 =====\n");
+  xs prefix = *xs_tmp("((("), suffix = *xs_tmp(")))");
+  xs_concat(&s1, &prefix, &suffix);
+  printf("After concat to s1, s2: %s %ld\n", xs_data(&s1),
+         XS_GET_REFCNT((&s1)));
+}
+
+int main() {
+  test_cpy();
+
+  return 0;
 }
